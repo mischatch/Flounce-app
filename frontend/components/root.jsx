373f813a20c42f16994ec5e85ef554342e324abf
Mikhail Cheburakhtin <mischa.tch@gmail.com>
2017-08-22 18:54:46 -0400

    keep fixing

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
-import App from './app';
+import App from './App';
 
 const Root = ({ store }) => (
-  <Provider store={store}>
+  <Provider store={ store }>
     <HashRouter>
       <App />
     </HashRouter>
